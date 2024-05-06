3eba4d36924005e57562d63b94fd6cd594ef7221
Amine <amin.ouni@neopolis-dev.com>
2024-05-06 13:37:41 +0100

    Update main layout with new components

--- src/components/app/side-bar.tsx
+++ src/components/app/side-bar.tsx
@@ -1,9 +1,11 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import {
   Home,
   LineChart,
+  LogOut,
   Package,
   Package2,
+  PanelLeft,
   Settings,
   ShoppingCart,
   Users2,
@@ -14,96 +16,141 @@ import {
   TooltipContent,
   TooltipTrigger,
 } from "@/components/ui/tooltip";
-type Props = {}
+import { Sheet, SheetContent, SheetTrigger } from "../ui/sheet";
+import { Button } from "../ui/button";
 
-const Sidebar = (props: Props) => {
+const sidebarItems = [
+  {
+    title: "Dashboard",
+    icon: <Home className="h-5 w-5" />,
+    path: "/",
+  },
+  {
+    title: "Orders",
+    icon: <ShoppingCart className="h-5 w-5" />,
+    path: "/orders",
+  },
+  {
+    title: "Products",
+    icon: <Package className="h-5 w-5" />,
+    path: "/products",
+  },
+  {
+    title: "Customers",
+    icon: <Users2 className="h-5 w-5" />,
+    path: "/customers",
+  },
+  {
+    title: "Analytics",
+    icon: <LineChart className="h-5 w-5" />,
+    path: "/analytics",
+  },
+];
+
+const Sidebar = () => {
+  const { pathname } = useLocation();
   return (
-    <aside className="fixed inset-y-0 left-0 z-10 hidden w-14 flex-col border-r bg-background sm:flex">
-    <nav className="flex flex-col items-center gap-4 px-2 sm:py-5">
-      <Link
-        to="#"
-        className="group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:h-8 md:w-8 md:text-base"
-      >
-        <Package2 className="h-4 w-4 transition-all group-hover:scale-110" />
-        <span className="sr-only">Nymera Inc</span>
-      </Link>
-      <Tooltip>
-        <TooltipTrigger asChild>
+    <>
+      <nav className="flex flex-col items-center gap-4 px-2 sm:py-5">
+        <Link
+          to="#"
+          className="group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:h-8 md:w-8 md:text-base"
+        >
+          <Package2 className="h-4 w-4 transition-all group-hover:scale-110" />
+          <span className="sr-only">Nymera Inc</span>
+        </Link>
+        {sidebarItems.map((item, index) => (
+          <Tooltip key={index}>
+            <TooltipTrigger asChild>
+              <Link
+                to={item.path}
+                className={`${
+                  item?.path === pathname &&
+                  "bg-primary-foreground text-primary"
+                } flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8`}
+              >
+                {item.icon}
+                <span className="sr-only">{item.title}</span>
+              </Link>
+            </TooltipTrigger>
+            <TooltipContent side="right">{item.title}</TooltipContent>
+          </Tooltip>
+        ))}
+      </nav>
+      <nav className="mt-auto flex flex-col items-center gap-4 px-2 sm:py-5">
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Link
+              to="#"
+              className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
+            >
+              <Settings className="h-5 w-5" />
+              <span className="sr-only">Settings</span>
+            </Link>
+          </TooltipTrigger>
+          <TooltipContent side="right">Settings</TooltipContent>
+        </Tooltip>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Link
+              to="#"
+              className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
+            >
+              <LogOut className="h-5 w-5" />
+              <span className="sr-only">Logout</span>
+            </Link>
+          </TooltipTrigger>
+          <TooltipContent side="right">Logout</TooltipContent>
+        </Tooltip>
+      </nav>
+    </>
+  );
+};
+
+const SidebarMobile = () => {
+  const { pathname } = useLocation();
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Button size="icon" variant="outline" className="sm:hidden">
+          <PanelLeft className="h-5 w-5" />
+          <span className="sr-only">Toggle Menu</span>
+        </Button>
+      </SheetTrigger>
+      <SheetContent side="left" className="sm:max-w-xs">
+        <nav className="grid gap-6 text-lg font-medium">
           <Link
-            to="#"
-            className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
+            to="/"
+            className="group flex h-10 w-10 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:text-base"
           >
-            <Home className="h-5 w-5" />
-            <span className="sr-only">Dashboard</span>
+            <Package2 className="h-5 w-5 transition-all group-hover:scale-110" />
+            <span className="sr-only">Nymera Inc</span>
           </Link>
-        </TooltipTrigger>
-        <TooltipContent side="right">Dashboard</TooltipContent>
-      </Tooltip>
-      <Tooltip>
-        <TooltipTrigger asChild>
+          {sidebarItems.map((item, index) => (
+            <Link
+              to={item.path}
+              key={index}
+              className={`${
+                item?.path === pathname
+                  ? "text-foreground"
+                  : "text-muted-foreground"
+              } flex items-center gap-4 px-2.5  hover:text-foreground`}
+            >
+              {item.icon}
+              {item.title}
+            </Link>
+          ))}
           <Link
             to="#"
-            className="flex h-9 w-9 items-center justify-center rounded-lg bg-accent text-accent-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
-          >
-            <ShoppingCart className="h-5 w-5" />
-            <span className="sr-only">Orders</span>
-          </Link>
-        </TooltipTrigger>
-        <TooltipContent side="right">Orders</TooltipContent>
-      </Tooltip>
-      <Tooltip>
-        <TooltipTrigger asChild>
-          <Link
-            to="#"
-            className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
-          >
-            <Package className="h-5 w-5" />
-            <span className="sr-only">Products</span>
-          </Link>
-        </TooltipTrigger>
-        <TooltipContent side="right">Products</TooltipContent>
-      </Tooltip>
-      <Tooltip>
-        <TooltipTrigger asChild>
-          <Link
-            to="#"
-            className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
-          >
-            <Users2 className="h-5 w-5" />
-            <span className="sr-only">Customers</span>
-          </Link>
-        </TooltipTrigger>
-        <TooltipContent side="right">Customers</TooltipContent>
-      </Tooltip>
-      <Tooltip>
-        <TooltipTrigger asChild>
-          <Link
-            to="#"
-            className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
+            className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
           >
             <LineChart className="h-5 w-5" />
-            <span className="sr-only">Analytics</span>
+            Settings
           </Link>
-        </TooltipTrigger>
-        <TooltipContent side="right">Analytics</TooltipContent>
-      </Tooltip>
-    </nav>
-    <nav className="mt-auto flex flex-col items-center gap-4 px-2 sm:py-5">
-      <Tooltip>
-        <TooltipTrigger asChild>
-          <Link
-            to="#"
-            className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
-          >
-            <Settings className="h-5 w-5" />
-            <span className="sr-only">Settings</span>
-          </Link>
-        </TooltipTrigger>
-        <TooltipContent side="right">Settings</TooltipContent>
-      </Tooltip>
-    </nav>
-  </aside>
-  )
-}
+        </nav>
+      </SheetContent>
+    </Sheet>
+  );
+};
 
-export default Sidebar
+export { Sidebar, SidebarMobile };
--- src/layouts/main-layout.tsx
+++ src/layouts/main-layout.tsx
@@ -1,21 +1,17 @@
-import { Link } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 import {
   Home,
   LineChart,
   Package,
   Package2,
   PanelLeft,
-  Settings,
   ShoppingCart,
   Users2,
 } from "lucide-react";
 
-
 import { Button } from "@/components/ui/button";
-
 import {
   DropdownMenu,
-  DropdownMenuCheckboxItem,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuLabel,
@@ -23,78 +19,47 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 
-import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipTrigger,
-} from "@/components/ui/tooltip";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import Sidebar from "@/components/app/side-bar";
+import {Sidebar, SidebarMobile} from "@/components/app/side-bar";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
 
 const MainLayout = () => {
   return (
     <div className="flex min-h-screen w-full flex-col bg-muted/40">
-      <Sidebar />
+      <aside className="fixed inset-y-0 left-0 z-10 hidden w-14 flex-col border-r bg-background sm:flex">
+        <Sidebar />
+      </aside>
       <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
         <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
-          <Sheet>
-            <SheetTrigger asChild>
-              <Button size="icon" variant="outline" className="sm:hidden">
-                <PanelLeft className="h-5 w-5" />
-                <span className="sr-only">Toggle Menu</span>
-              </Button>
-            </SheetTrigger>
-            <SheetContent side="left" className="sm:max-w-xs">
-              <nav className="grid gap-6 text-lg font-medium">
-                <Link
-                  to="#"
-                  className="group flex h-10 w-10 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:text-base"
-                >
-                  <Package2 className="h-5 w-5 transition-all group-hover:scale-110" />
-                  <span className="sr-only">Acme Inc</span>
-                </Link>
-                <Link
-                  to="#"
-                  className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
-                >
-                  <Home className="h-5 w-5" />
-                  Dashboard
-                </Link>
-                <Link
-                  to="#"
-                  className="flex items-center gap-4 px-2.5 text-foreground"
-                >
-                  <ShoppingCart className="h-5 w-5" />
-                  Orders
-                </Link>
-                <Link
-                  to="#"
-                  className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
-                >
-                  <Package className="h-5 w-5" />
-                  Products
-                </Link>
-                <Link
-                  to="#"
-                  className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
-                >
-                  <Users2 className="h-5 w-5" />
-                  Customers
-                </Link>
-                <Link
-                  to="#"
-                  className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
-                >
-                  <LineChart className="h-5 w-5" />
-                  Settings
-                </Link>
-              </nav>
-            </SheetContent>
-          </Sheet>
+          <SidebarMobile />
+          <Breadcrumb className="hidden md:flex">
+            <BreadcrumbList>
+              <BreadcrumbItem>
+                <BreadcrumbLink asChild>
+                  <Link to="#">Dashboard</Link>
+                </BreadcrumbLink>
+              </BreadcrumbItem>
+              <BreadcrumbSeparator />
+              <BreadcrumbItem>
+                <BreadcrumbLink asChild>
+                  <Link to="#">Products</Link>
+                </BreadcrumbLink>
+              </BreadcrumbItem>
+              <BreadcrumbSeparator />
+              <BreadcrumbItem>
+                <BreadcrumbPage>All Products</BreadcrumbPage>
+              </BreadcrumbItem>
+            </BreadcrumbList>
+          </Breadcrumb>
 
-          <div className="relative ml-auto flex-1 md:grow-0">
-          </div>
+          <div className="relative ml-auto flex-1 md:grow-0"></div>
           <DropdownMenu>
             <DropdownMenuTrigger asChild>
               <Button
@@ -121,7 +86,9 @@ const MainLayout = () => {
             </DropdownMenuContent>
           </DropdownMenu>
         </header>
-        <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8 lg:grid-cols-3 xl:grid-cols-3"></main>
+        <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8 lg:grid-cols-3 xl:grid-cols-3">
+          <Outlet />
+        </main>
       </div>
     </div>
   );
