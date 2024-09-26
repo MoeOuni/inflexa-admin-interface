6280561be5d1834f0070db63f0aa2e0d7f1cfed4
Moe Ouni <moe.ouni20@gmail.com>
2024-09-26 17:06:55 +0100

    refactor: Update AuthContextProvider and api-clients

--- src/api/api-clients.ts
+++ src/api/api-clients.ts
@@ -1,25 +1,58 @@
 import axios from 'axios';
+import { toast } from 'sonner';
 
 const apiClient = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 apiClient.interceptors.request.use((config) => {
-    const token = localStorage.getItem('x-auth-token');
+  const token = localStorage.getItem('x-auth-token');
 
-    if (token) {
-        config.headers.Authorization = `Bearer ${token.slice(1, -1)}`;
-    }
-    return config;
+  if (token) {
+    config.headers.Authorization = `Bearer ${token.slice(1, -1)}`;
+  }
+  return config;
 });
+
+let isUnauthorized = false;
+
+apiClient.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      if (!isUnauthorized) {
+        isUnauthorized = true;
+
+        // Redirect to login page
+        toast.error('Unauthorized. Please log in again.', {
+          action: {
+            label: 'Log in again',
+            onClick: () => {
+              localStorage.clear();
+              sessionStorage.clear();
+              window.location.href = '/login'; // Clear session and localStorage
+            },
+          },
+          duration: 100000,
+        });
+
+        return Promise.reject(new Error('Unauthorized. Please log in again.'));
+      } else {
+        // Cancel all other API calls
+        return new axios.Cancel('Unauthorized. API call cancelled.');
+      }
+    }
+    return Promise.reject(error);
+  }
+);
 
 export { apiClient };
 
 export const API_METHODS = {
-    GET: 'GET',
-    POST: 'POST',
-    PUT: 'PUT',
-    PATCH: 'PATCH',
-    DELETE: 'DELETE',
-    OPTIONS: 'OPTIONS',
+  GET: 'GET',
+  POST: 'POST',
+  PUT: 'PUT',
+  PATCH: 'PATCH',
+  DELETE: 'DELETE',
+  OPTIONS: 'OPTIONS',
 } as const;
